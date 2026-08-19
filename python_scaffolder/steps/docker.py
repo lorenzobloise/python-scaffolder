9d7fa56d82bab7925bd855f0cc5a30014446c526
lorenzobloise <l.bloise@outlook.it>
2026-08-19 21:14:22 +0200

    feat(docker): create entry point file if does not exist

--- python_scaffolder/steps/docker.py
+++ python_scaffolder/steps/docker.py
@@ -18,12 +18,18 @@ class Docker(Step):
             .joinpath("assets", "docker", "template")
         )
 
+    def _create_entry_point_file(self, path: Path, entry_point: str):
+        entry_point_path: Path = path / entry_point
+        entry_point_path.parent.mkdir(parents=True, exist_ok=True)
+        entry_point_path.touch(exist_ok=True)
+
     def run(self, path: Path, config: dict) -> None:
         digest: str = config.get("digest", "") or ""
         if digest:
             digest = f"@{digest}"
         python_version: str = _get_python_version(path) or _get_python_version_from_executable(sys.executable)
         entry_point: str = config.get("entry_point", "main.py")
+        self._create_entry_point_file(path, entry_point)
         template: Path = self._dockerfile_template_path
         file_content: str = template.read_text().format(
             version=python_version,
@@ -32,4 +38,4 @@ class Docker(Step):
         )
         dockerfile_path: Path = path / "Dockerfile"
         dockerfile_path.write_text(file_content)
-        self.success("Created Dockerfile")
+        self.success(f"Created Dockerfile and entry point {entry_point}")
